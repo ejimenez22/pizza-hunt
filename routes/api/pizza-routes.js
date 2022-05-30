d1b9894773565bbaf01d9fc3f26ab19f81134bf0
ejimenez22 <ejimenez2292@gmail.com>
2022-05-29 21:13:35 -0500

    used code snapshot to fix errors

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,22 +1,23 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
-} = require('../../controllers/pizza-controller')
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
-//api/pizzas
-router 
-.route('/')
-.get(getAllPizza)
-.post(createPizza)
-
+// /api/pizzas
 router
-.route('/:id')
-.get(getPizzaById)
-.put(updatePizza)
-.delete(deletePizza)
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
 
-module.exports = router
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
